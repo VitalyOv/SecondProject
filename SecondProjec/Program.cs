8ed4c69a7e9e4c46f4c2f50573cb4e8062f56d6e
vitaxaov <johndoe@example.com>
2024-03-19 20:02:42 +0300

    dsafsdfs

--- SecondProjec/Program.cs
+++ SecondProjec/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!123");sdfsdfsdfsdf
+Console.WriteLine("Hello, World!123");
+
+1231232131
